234875295a8f6e93c9261c9da9759874eec4507c
Jakub Sarnowski <j.sarnowski@linux.pl>
2020-11-07 03:05:18 +0100

    Removed http-proxy references for the time being

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
 const http = require('http');
-const httpProxy = require('http-proxy');
 const path = require('path');
 const app = require('./index');
-
-const dotenv = require('dotenv');
-dotenv.config();
-
-httpProxy.createProxyServer({target:process.env.SERVER_URL}).listen(process.env.SERVER_PROXY);
 
 async function server_init() {
     const port = process.env.PORT || 3300;
