82cf621c125443237ece0f48c22f83a015c418c3
Jakub Sarnowski <j.sarnowski@linux.pl>
2020-11-07 03:02:14 +0100

    Changed format for req.remoteAddress, which now resolves an issue with proxy

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,7 +1,7 @@
 module.exports = {
     ensureAuthenticated: function(req, res, next) {
         if(req.isAuthenticated()) {
-            console.log(req.connection.remoteAddress.replace('::ffff:', '') + ' - Access to /dashboard granted to authorized user');
+            console.log(req.headers['x-forwarded-for'] || req.connection.remoteAddress.replace('::ffff:', '') + ' - Access to /dashboard granted to authorized user');
             return next();
         }
         req.flash('error_msg', "Please login to view this resource");
