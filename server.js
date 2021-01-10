3215dd4a1c331878a56d8a46ed1ca12dd2d25820
Jakub Sarnowski <j.sarnowski@linux.pl>
2021-01-10 13:13:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,21 @@ const http = require('http');
 const path = require('path');
 const app = require('./index');
 
-const { I18n } = require('i18n')
+//const { I18n } = require('i18n')
  
-const i18n = new I18n({
-  locales: ['en', 'pl'],
-  directory: path.join(__dirname, 'locales')
-})
+//const i18n = new I18n({
+// locales: ['en', 'pl'],
+//  directory: path.join(__dirname, 'locales')
+//})
+
+const options = {
+  key: fs.readFileSync('web-organizer.org.pl.crt'),
+  cert: fs.readFileSync('web-organizer.org.pl.key')
+};
 
 async function server_init() {
     const port = process.env.PORT || 3300;
-    const server = http.createServer(app);
+    const server = http.createServer(options, app);
     server.listen(port);
     console.log(`Server is listening on port ${port}`);
 }
