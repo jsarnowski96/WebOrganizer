3e961e331d44583a58d52fbcfa890589784ce9c6
Jakub Sarnowski <j.sarnowski@linux.pl>
2021-01-19 21:53:19 +0100

    .

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const app = require('./index');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const port = process.env.PORT || 3300;
-const server = http.createServer(app);
-server.listen(port);
-console.log(`Server is listening on port ${port}`);
+async function server_init() {
+    const port = process.env.PORT || 3300;
+    const server = http.createServer(app);
+    server.listen(port);
+    console.log(`Server is listening on port ${port}`);
+}
+
+server_init();
