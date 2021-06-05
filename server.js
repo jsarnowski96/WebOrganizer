6b50a7a1ef3b400c6fa2d59faff986be918fa935
Jakub Sarnowski <j.sarnowski@linux.pl>
2021-06-05 16:20:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,8 @@ const app = require('./index');
 const dotenv = require('dotenv');
 dotenv.config();
 
-async function server_init() {
-    const port = process.env.PORT || 2087;
-    const server = http.createServer(app);
-    server.listen(port);
-    console.log(`Server is listening on port ${port}`);
-}
+const port = process.env.PORT || 2087;
+const server = http.createServer(app);
+server.listen(port);
 
-await server_init();
+console.log(`Server is listening on port ${port}`);
